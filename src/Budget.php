4806c587f2b912a806e19539b8d7f789c47ba8de
Roger Rezende <javielrezende@gmail.com>
2021-12-01 23:11:38 -0300

    :poop: Problem without state
    Many conditionals and need for rules to change budget states

--- src/Budget.php
+++ src/Budget.php
@@ -2,8 +2,31 @@
 
 namespace PersonalProjects\DesignPattern;
 
+use DomainException;
+
 class Budget
 {
     public float $value;
     public int $quantityItens;
+    public string $state;
+
+    public function applyExtraDiscount(): void
+    {
+        $this->value -= $this->calculateExtraDiscount();
+    }
+
+    private function calculateExtraDiscount (): float
+    {
+        if ($this->state === 'ON_APPROVAL') {
+            return $this->value * .05;
+        }
+        
+        if ($this->state === 'APPROVED') {
+            return $this->value * .02;
+        }
+
+        throw new DomainException(
+            "Approved and finalized budgets cannot receive discount"
+        );
+    }
 }
